05dba504657fc2e190338a9d7a0a0ea569769dfe
markpal <marekp1980@gmail.com>
2024-09-23 17:16:57 +0200

    k loop is necessary?

--- main.cpp
+++ main.cpp
@@ -116,9 +116,10 @@ int _i;
     int N = 7;
     for (i = N-1; i >= 0; i--) {
         for (j = i+1; j < N; j++) {
-            for (k = 0; k < j-i; k++) {
-                S[i][j] = max(S[i][k+i] + S[k+i+1][j], S[i][j]);
-            }
+           // for (k = 0; k < j-i; k++) {
+           //     S[i][j] = max(S[i][k+i] + S[k+i+1][j], S[i][j]);
+           // }
+            S[i][j] = max(S[i][j], max(S[i+1][j], S[i][j-1]));  /// ?????
             S[i][j] = max(S[i][j], S[i+1][j-1] + paired(seq[i],seq[j]));
 
            // cout << i << "|" << j << "|" << seq[i] << seq[j] << "|" << S[i][j] << endl;
@@ -137,24 +138,7 @@ int _i;
     }
 
 
-/*
-    for (int c0 = 0; c0 <= N; c0 += 1){
-        cout << "synch" << endl;
-        for (int c1 = c0; c1 < N; c1 += 1)
-            cout << -c0 + c1 << " " << c1 << endl;
-    }
-    */
-
-
-
-   /* for (int c0 = 0; c0 < 2; c0 += 1){
-        cout << "synch" << endl;
-        for (int c1 = c0; c1 < 2; c1 += 1)
-            for (int c2 = max(0, 5 * c0 - c1 + 1); c2 < min(4 * c0 + c1, N + 4 * c0 - 4 * c1); c2 += 1)
-                for (int c3 = max(4 * c1, -4 * c0 + 4 * c1 + c2); c3 < min(min(N, 5 * c1), -5 * c0 + 5 * c1 + c2); c3 += 1)
-                    //   (-c0 + c1, c1, -c2 + c3, c3);
-                    cout << -c0 + c1 << " " << c1 << ":" << -c2 + c3 << " " << c3 << endl;
-    }*/
+cout << "barvinok" << endl;
     for (int c0 = 0; c0 <= N/4; c0 += 1) {
         cout << "synch tile" << endl;
         for (int c1 = c0; c1 <= N / 4; c1 += 1)
@@ -166,9 +150,89 @@ int _i;
             }
     }
 
+cout << "parallel dapt" << endl;
+    for (int w0 = 1; w0 < N; w0++) {
+        cout << "synch" << endl;
+        for (int h0 = -N + w0 + 1; h0 <= 0; h0++) {
+            for (int i3 = 0; i3 < w0; i3++) {
+              //  cpu_S[-h0][w0 - h0] = MAX(cpu_S[-h0][-h0 + i3] + cpu_S[-h0 + i3 + 1][w0 - h0], cpu_S[-h0][w0 - h0]);
+            }
+            //cpu_S[-h0][w0 - h0] = MAX(cpu_S[-h0][w0 - h0], cpu_S[-h0 + 1][w0 - h0 - 1] + isPair_host(-h0, w0 - h0));
+          cout << -h0 << " " << w0 - h0 << endl;
+        }
+    }
+    cout << endl;
+    cout << "parallel pluto" << endl;
+    int t1, t2, t3, t4, t5, t6, t7, t9;
+    int lb, ub, lbp, ubp, lb2, ub2;
+    register int lbv, ubv;
+/* Start of CLooG code */
+    for (t2=1;t2<=6;t2++) {
+        lbp=t2;
+        ubp=6;
+        cout << "synch" << endl;
+//#pragma omp parallel for private(lbv,ubv,t5,t6,t7)
+        for (t4=lbp;t4<=ubp;t4++) {
+            for (t6=0;t6<=t2-1;t6++) {
+              //  S[(-t2+t4)][t4] = S[(-t2+t4)][t6+(-t2+t4)] + S[t6+(-t2+t4)+1][t4] + S[(-t2+t4)][t4];;
+            }
+            //S[(-t2+t4)][t4] = S[(-t2+t4)][t4] + S[(-t2+t4)+1][t4-1];;
+            cout << -t2+t4 << " " << t4 << endl;
+        }
+    }
 
-
-
+    for (t2=0;t2<=3;t2++) {
+        lbp=t2;
+        ubp=3;
+#pragma omp parallel for private(lbv,ubv,t5,t6,t7,t8,t9,t10)
+        for (t4=lbp;t4<=ubp;t4++) {
+            for (t5=max(-5,2*t2-2*t4);t5<=min(0,2*t2-2*t4+1);t5++) {
+                for (t7=max(2*t4,-t5+1);t7<=min(6,2*t4+1);t7++) {
+                    for (t9=0;t9<=t5+t7-1;t9++) {
+                        S[-t5][t7] = S[-t5][t9+-t5] + S[t9+-t5+1][t7] + S[-t5][t7];;
+                    }
+                    S[-t5][t7] = S[-t5][t7] + S[-t5+1][t7-1];;
+                }
+            }
+        }
+    }
+/*
+    cout << "parallel tile pluto" << endl;
+    for (t2=-1;t2<=1;t2++) {
+        lbp=max(0,t2);
+        ubp=1;
+        cout << "synch" << endl;
+//#pragma omp parallel for private(lbv,ubv,t5,t6,t7,t8,t9,t10)
+        for (t4=lbp;t4<=ubp;t4++) {
+            for (t5=max(max(-5,4*t2-4*t4),-4*t4-2);t5<=min(0,4*t2-4*t4+3);t5++) {
+                for (t7=max(4*t4,-t5+1);t7<=min(6,4*t4+3);t7++) {
+                    for (t9=0;t9<=t5+t7-1;t9++) {
+                     //   S[-t5][t7] = S[-t5][t9+-t5] + S[t9+-t5+1][t7] + S[-t5][t7];;
+                    }
+                   // S[-t5][t7] = S[-t5][t7] + S[-t5+1][t7-1];;
+                   cout << t2 << " " << t4 << ":" << -t5 << " " << t7 << endl;
+                }
+            }
+        }
+    }*/
+    cout << "parallel tile pluto" << endl;
+    for (t2=0;t2<=3;t2++) {
+        lbp=t2;
+        ubp=3;
+        cout << "synch" << endl;
+//#pragma omp parallel for private(lbv,ubv,t5,t6,t7,t8,t9,t10)
+        for (t4=lbp;t4<=ubp;t4++) {
+            for (t5=max(-5,2*t2-2*t4);t5<=min(0,2*t2-2*t4+1);t5++) {
+                for (t7=max(2*t4,-t5+1);t7<=min(6,2*t4+1);t7++) {
+                    for (t9=0;t9<=t5+t7-1;t9++) {
+                      //  S[-t5][t7] = S[-t5][t9+-t5] + S[t9+-t5+1][t7] + S[-t5][t7];;
+                    }
+                 //   S[-t5][t7] = S[-t5][t7] + S[-t5+1][t7-1];;
+                    cout << t2 << " " << t4 << ":" << -t5 << " " << t7 << " uses " << -t5+1 << "|" << t7-1 << endl;
+                }
+            }
+        }
+    }
 
     return 0;
 }
